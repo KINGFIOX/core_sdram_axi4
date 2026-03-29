321b68ef7ac73226f4e8bf7210e0c9b52b691336
wangfiox <wangfiox@gmail.com>
2026-03-29 17:12:53 +0800

    nix

--- flake.nix
+++ flake.nix
@@ -1,53 +1,44 @@
 {
   description = "core_sdram_axi4 开发环境 ( Mill 0.12、Chisel、Verilator、SystemC )";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-  outputs = { self, nixpkgs }:
-    let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-      pkgsFor = system: import nixpkgs {
-        inherit system;
-        config = { };
-        overlays = [
-          (import ./nix/overlay.nix)
-        ];
-      };
-    in
-    {
-      devShells = forAllSystems (system:
-        let
-          pkgs = pkgsFor system;
-          verilator = pkgs.verilator;
-          systemc = pkgs.systemc;
-        in
-        {
-          default = pkgs.mkShell {
-            name = "core-sdram-axi4";
-            nativeBuildInputs = with pkgs; [
-              # chisel
-              mill_0_12_4
-              jdk17
-              metals
-              circt
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config = { };
+          overlays = [
+            (import ./nix/overlay.nix)
+          ];
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          name = "core-sdram-axi4";
+          packages = with pkgs; [
+            # chisel
+            mill_0_12_4
+            jdk21
+            metals
+            circt
 
-              # gcc
-              verilator
-              systemc
-              ccache
-              gcc
-              gdb
+            # gcc
+            verilator
+            systemc
 
-              #
-              gtkwave
-            ];
-            shellHook = ''
-              export VERILATOR_SRC="${verilator}/share/verilator/include"
-              export SYSTEMC_HOME="${systemc}"
-              export SYSTEMC_LIBDIR="${systemc}/lib"
-            '';
-          };
-        });
-    };
+            #
+            gtkwave
+          ];
+
+          VERILATOR_SRC = "${pkgs.verilator}/share/verilator/include";
+          SYSTEMC_HOME = "${pkgs.systemc}";
+          SYSTEMC_LIBDIR = "${pkgs.systemc}/lib";
+        };
+      }
+    );
 }
